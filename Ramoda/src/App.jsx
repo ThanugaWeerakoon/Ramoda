2e404ba9596eb69240028aba52c959702a8033f2
ThanugaWeerakoon <136422410+ThanugaWeerakoon@users.noreply.github.com>
2024-08-19 15:27:54 +0530

    slider

--- Ramoda/src/App.jsx
+++ Ramoda/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from './Navbar';
 import Home from './Home'
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 const App = () => {
   return (
--- Ramoda/src/Home.jsx
+++ Ramoda/src/Home.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
+import Slider from 'react-slick';
+import slider1 from './assets/slide1.jpg';
 
 const Home = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000, 
+  };
 
-export default Home
+  return (
+    <div className="p-0">
+      <Slider {...settings}>
+        <div>
+          <img src={slider1} alt="Slide 1" className="w-full" />
+        </div>
+        <div>
+          <img src={slider1} alt="Slide 1" className="w-full" />
+        </div>
+        <div>
+          <img src={slider1} alt="Slide 1" className="w-full" />
+        </div>
+        <div>
+          <img src={slider1} alt="Slide 1" className="w-full" />
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default Home;
