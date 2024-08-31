f611b3b41ecb3e6973246c8a5dc4d1e71e769d39
ThanugaWeerakoon <136422410+ThanugaWeerakoon@users.noreply.github.com>
2024-08-31 17:28:57 +0530

    navbar

--- Ramoda/src/Navbar.jsx
+++ Ramoda/src/Navbar.jsx
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'; 
+import cartpng from './assets/cart.png'
 
 const Navbar = () => {
   const [scroll, setScroll] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setScroll(true);
-      } else {
-        setScroll(false);
-      }
+      setScroll(window.scrollY > 0);
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -20,17 +17,23 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className={`bg-black text-white p-5 fixed w-full top z-50 transition-all duration-300 ${scroll ? 'shadow-lg' : ''}`}>
-      <div className="mx-auto flex justify-between items-center px-6">
-        <div className="text-3xl font-bold">
+    <nav className={`bg-black text-white p-6 fixed w-full top-0 z-50 transition-all duration-300 ${scroll ? 'shadow-lg' : 'shadow-md'}`}>
+      <div className=" mx-auto flex items-center justify-start">
+        <div className="text-3xl font-bold leading-tight mr-12">
           RA-MÓDA
         </div>
-        <div className="flex space-x-12 text-lg font-mono font-bold">
-          <Link to="/" className="hover:text-gray-400">HOME</Link> 
-          <Link to="/men" className="hover:text-gray-400">MEN</Link> 
-          <Link to="/women" className="hover:text-gray-400">WOMEN</Link> 
-          <Link to="/Aboutus" className="hover:text-gray-400">ABOUT US</Link>
+        <div className="flex space-x-8 font-semibold">
+          <Link to="/" className="hover:text-gray-300 transition-colors duration-300">HOME</Link>
+          <Link to="/men" className="hover:text-gray-300 transition-colors duration-300">MEN</Link>
+          <Link to="/women" className="hover:text-gray-300 transition-colors duration-300">WOMEN</Link>
+          <Link to="/aboutus" className="hover:text-gray-300 transition-colors duration-300">ABOUT US</Link>
+         
         </div>
+
+        <div className="flex items-center ml-auto">
+          <img src={cartpng} alt="Cart" className="w-10 h-10 object-contain cursor-pointer hover:opacity-75 transition-opacity duration-300" />
+        </div>
+
       </div>
     </nav>
   );
