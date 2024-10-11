cbea56c9dbbc548bec334d2defb16bfd40097f68
ThanugaWeerakoon <136422410+ThanugaWeerakoon@users.noreply.github.com>
2024-10-12 01:09:36 +0530

    Shop

--- src/shop.js
+++ src/shop.js
@@ -1,24 +1,12 @@
-import product1 from './assets/products/1.jpg';
-import product2 from './assets/products/2.jpg';
-import product3 from './assets/products/3.jpg';
-import product4 from './assets/products/4.jpg';
-import product5 from './assets/products/5.jpg';
-import product6 from './assets/products/6.jpg';
-import product7 from './assets/products/7.jpg';
-import product8 from './assets/products/8.jpg';
-import product9 from './assets/products/9.jpg';
-import product10 from './assets/products/10.jpg';
-import product11 from './assets/products/11.jpg';
-import product12 from './assets/products/12.jpg';
 
-import latest1 from './assets/latest/Product1.jpg';
-import latest2 from './assets/latest/Product2.jpg';
-import latest3 from './assets/latest/Product3.jpg';
-import latest4 from './assets/latest/Product4.jpg';
-import latest5 from './assets/latest/Product5.jpg';
-import latest6 from './assets/latest/Product6.jpg';
-import latest7 from './assets/latest/Product7.jpg';
-import latest8 from './assets/latest/Product8.jpg';
+import latest1 from './assets/latest/OceanBlue.jpg';
+import latest2 from './assets/latest/Hotpink.jpg';
+import latest3 from './assets/latest/Lightpink.jpg';
+import latest4 from './assets/latest/Biege.jpg';
+import latest5 from './assets/latest/Blue.jpg';
+import latest6 from './assets/latest/Black.jpg';
+import latest7 from './assets/latest/SandStone.jpg';
+import latest8 from './assets/latest/Red.jpg';
 
 import M1 from './assets/men/Biege.jpg';
 import M2 from './assets/men/Black.jpg';
@@ -55,30 +43,16 @@ import D6 from './assets/dress/6.jpg';
 
 const products = [
   // Latest products
-  { id: 101, name: 'Latest 1', price: '2150', image: latest1, isLatest: true },
-  { id: 102, name: 'Latest 2', price: '2150', image: latest2, isLatest: true },
-  { id: 103, name: 'Latest 3', price: '2150', image: latest3, isLatest: true },
-  { id: 104, name: 'Latest 4', price: '2150', image: latest4, isLatest: true },
-  { id: 105, name: 'Latest 5', price: '2150', image: latest5, isLatest: true },
-  { id: 106, name: 'Latest 6', price: '2150', image: latest6, isLatest: true },
-  { id: 107, name: 'Latest 7', price: '2150', image: latest7, isLatest: true },
-  { id: 108, name: 'Latest 8', price: '2150', image: latest8, isLatest: true },
+  { id: 101, name: 'Ocean Blue', price: '2150', image: latest1, isLatest: true },
+  { id: 102, name: 'Hot Pink', price: '2150', image: latest2, isLatest: true },
+  { id: 103, name: 'Light Pink', price: '2150', image: latest3, isLatest: true },
+  { id: 104, name: 'Biege', price: '2150', image: latest4, isLatest: true },
+  { id: 105, name: 'Blue', price: '2150', image: latest5, isLatest: true },
+  { id: 106, name: 'Black', price: '2150', image: latest6, isLatest: true },
+  { id: 107, name: 'Sand Stone', price: '2150', image: latest7, isLatest: true },
+  { id: 108, name: 'Red', price: '2150', image: latest8, isLatest: true },
   
-  // Regular products
-  { id: 1, name: 'Product 1', price: '2150', image: product1, isLatest: false },
-  { id: 2, name: 'Product 2', price: '2150', image: product2, isLatest: false },
-  { id: 3, name: 'Product 3', price: '2150', image: product3, isLatest: false },
-  { id: 4, name: 'Product 4', price: '2150', image: product4, isLatest: false },
-  { id: 5, name: 'Product 5', price: '2150', image: product5, isLatest: false },
-  { id: 6, name: 'Product 6', price: '2150', image: product6, isLatest: false },
-  { id: 7, name: 'Product 7', price: '2150', image: product7, isLatest: false },
-  { id: 8, name: 'Product 8', price: '2150', image: product8, isLatest: false },
-  { id: 9, name: 'Product 9', price: '2150', image: product9, isLatest: false },
-  { id: 10, name: 'Product 10', price: '2150', image: product10, isLatest: false },
-  { id: 11, name: 'Product 11', price: '2150', image: product11, isLatest: false },
-  { id: 12, name: 'Product 12', price: '2150', image: product12, isLatest: false },
-
-  
+ 
   //Men Products
 
   { id: 31, name: 'Biege', price: '2150', image: M1, isMen: true },
